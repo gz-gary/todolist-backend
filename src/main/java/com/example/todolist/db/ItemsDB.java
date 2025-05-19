2f558b61de4121a7011fa08b0e3fd0f1dc6a1633
gryz <jinlizhong@smail.nju.edu.cn>
2025-05-19 11:53:23 +0800

    Implement: Interface of ItemsDB

---
+++ src/main/java/com/example/todolist/db/ItemsDB.java
@@ -0,0 +1,12 @@
+package com.example.todolist.db;
+
+import com.example.todolist.model.Item;
+
+import java.util.List;
+
+public interface ItemsDB {
+    public List<Item> getItems();
+    public Item getItemById(int id);
+    public int createItem(String title);
+    public boolean finishItem(int id, boolean finished);
+}
