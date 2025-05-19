fd2119d767d24d0a2b6eacd0b07318d6d4f487fa
gryz <jinlizhong@smail.nju.edu.cn>
2025-05-19 11:53:43 +0800

    Implement: MockItemsDB in memory for tests

---
+++ src/test/java/com/example/todolist/MockItemsDB.java
@@ -0,0 +1,43 @@
+package com.example.todolist;
+
+import com.example.todolist.db.ItemsDB;
+import com.example.todolist.model.Item;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class MockItemsDB implements ItemsDB {
+    private int lastId = 0;
+    private List<Item> items = new LinkedList<>();
+
+    @Override
+    public List<Item> getItems() {
+        return items;
+    }
+
+    @Override
+    public Item getItemById(int id) {
+        for (Item item : items) if (item.getId() == id) return item;
+        return null;
+    }
+
+    @Override
+    public int createItem(String title) {
+        items.add(
+            new Item(lastId, LocalDateTime.now(), title, false)
+        );
+        return lastId++;
+    }
+
+    @Override
+    public boolean finishItem(int id, boolean finished) {
+        Item item = getItemById(id);
+        if (item == null) return false;
+        item.setFinished(finished);
+        return true;
+    }
+}
