324217965809b1b40811c49a0a0348b8fee0e9f8
gryz <jinlizhong@smail.nju.edu.cn>
2025-05-20 15:47:09 +0800

    Fix: configuration completed

--- src/main/java/com/example/todolist/config/TodolistApplicationConfig.java
+++ src/main/java/com/example/todolist/config/TodolistApplicationConfig.java
@@ -1,13 +1,23 @@
 package com.example.todolist.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class TodolistApplicationConfig {
     @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate();
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
