09cdd1a8fd5a64110eb447c261f12a5d185c3761
gryz <jinlizhong@smail.nju.edu.cn>
2025-05-24 18:42:41 +0800

    Fix: allow CORS

--- src/main/java/com/example/todolist/controller/ItemsController.java
+++ src/main/java/com/example/todolist/controller/ItemsController.java
@@ -6,6 +6,7 @@ import com.example.todolist.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/items")
 public class ItemsController {
